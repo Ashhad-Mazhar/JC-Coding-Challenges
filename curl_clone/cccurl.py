892585132747e217496fc338cdb250e323c35572
Ashhad <mianashhad7@gmail.com>
2024-08-30 21:41:03 +0500

    Completed step 4 (Added support for POST requests)

--- curl_clone/cccurl.py
+++ curl_clone/cccurl.py
@@ -3,12 +3,7 @@ import argparse
 from urllib.parse import urlparse
 
 def main():
-    parser = argparse.ArgumentParser(description='CLI utility to send requests')
-    parser.add_argument('-v', '--verbose', action='store_true',
-                        help='Enable verbose output')
-    parser.add_argument(
-        '-X', dest='request_method', type=str, help='Request method to use')
-    parser.add_argument('url', type=str, help='The URL to send request to')
+    parser = create_args_parser()
     args = parser.parse_args()
     if not args.request_method:
         args.request_method = 'GET'
@@ -18,7 +13,9 @@ def main():
         'Accept: */*',
         'Connection: close'
     ]
-    request = create_request(parsed_url, args.request_method, headers)
+    if args.request_header:
+        headers.append(args.request_header)
+    request = create_request(parsed_url, args.request_method, headers, args.request_payload)
     response = send_request(request, parsed_url['host'], parsed_url['port'])
 
     split_response = response.split('\r\n\r\n')
@@ -32,6 +29,23 @@ def main():
         print(verbose_response_headers)
 
     print(response_body)
+
+def create_args_parser() -> argparse.ArgumentParser:
+    '''
+    Returns a parser for handling CLI arguments and flags
+    '''
+    parser = argparse.ArgumentParser(description='CLI utility to send requests')
+    parser.add_argument('-v', '--verbose', action='store_true',
+                        help='Enable verbose output')
+    parser.add_argument(
+        '-X', dest='request_method', type=str, help='Request method to use')
+    parser.add_argument('url', type=str, help='The URL to send request to')
+    parser.add_argument(
+        '-d', dest='request_payload', type=str,
+        help='Payload to be sent with the request')
+    parser.add_argument('-H', dest='request_header',
+                        type=str, help='Header to be sent with the request')
+    return parser
 
 def parse_url(url: str) -> dict:
     '''
@@ -52,13 +66,17 @@ def parse_url(url: str) -> dict:
         'path': path,
     }
 
-def create_request(parsed_url: dict, request_method: str, headers: list[str]) -> str:
+def create_request(parsed_url: dict, request_method: str, headers: list[str], request_payload: str) -> str:
     '''
-    Returns a GET request in string form for the given URL
+    Returns an HTTP request in string form for the given URL
     '''
     http_version = 'HTTP/1.1'
     start_line = f'{request_method.upper()} {parsed_url['path']} {http_version}'
+    if request_payload:
+        headers.append(f'Content-Length: {len(request_payload)}')
     request = f'{start_line}\r\n' + '\r\n'.join(headers) + '\r\n\r\n'
+    if request_payload:
+        request += request_payload
     return request
 
 def send_request(request: str, host: str, port: int) -> str:
@@ -73,12 +91,18 @@ def send_request(request: str, host: str, port: int) -> str:
     return data.decode('utf-8')
 
 def get_verbose_response_headers(response_headers: str) -> str:
+    '''
+    Appends a < symbol before each header for verbose mode
+    '''
     response_headers_lines = response_headers.splitlines()
     response_headers_lines = [f'< {line}' for line in response_headers_lines]
     verbose_response_headers = '\r\n'.join(response_headers_lines) + f'\r\n<'
     return verbose_response_headers
 
 def get_verbose_request(request: str) -> str:
+    '''
+    Appends a > symbol before each header for verbose mode
+    '''
     request_lines = request.splitlines()
     request_lines = [f'> {line}' for line in request_lines]
     verbose_request = '\r\n'.join(request_lines)
